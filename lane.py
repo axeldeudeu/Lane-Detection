f1ea6953ae19b52df96238476bb62194468ee3ef
Siddharth Telang <stelang@umd.edu>
2021-06-16 11:00:50 -0400

    add function to do color thresholding

--- lane.py
+++ lane.py
@@ -32,3 +32,26 @@ class LaneDetection:
         warped = cv2.warpPerspective(image, H, (300, 300))
         return warped, H
 
+    # function to do HLS color thresholding
+    def doColorThresholding(self, warped):
+        #https://www.w3schools.com/colors/colors_hsl.asp
+
+        hls = cv2.cvtColor(warped, cv2.COLOR_BGR2HLS).astype(np.float)
+
+        #Seperate yellow
+        lower_yellow = np.array([20,90,55],dtype=np.uint8)
+        upper_yellow = np.array([45,200,255],dtype=np.uint8)
+        yellow_mask = cv2.inRange(hls,lower_yellow,upper_yellow)
+        yellow_line = cv2.bitwise_and(hls, hls, mask=yellow_mask).astype(np.uint8)
+
+        #Seperate White
+        lower_white = np.array([0,200,0],dtype=np.uint8)
+        upper_white = np.array([255,255,255],dtype = np.uint8)
+        white_mask = cv2.inRange(hls,lower_white,upper_white)
+        white_line = cv2.bitwise_and(hls, hls, mask=white_mask).astype(np.uint8)
+
+        mask = cv2.bitwise_or(white_mask,yellow_mask)
+        preprocessed_hls = cv2.bitwise_or(yellow_line, white_line)
+        preprocessed_img = cv2.bitwise_and(warped, warped, mask=mask).astype(np.uint8)
+
+        return mask
